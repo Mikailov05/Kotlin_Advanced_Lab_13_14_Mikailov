5a2dec0cb37347060206c2913e6c073e6e335d49
Mikailov05 <mikailov_akhmed@bk.ru>
2026-01-28 18:29:02 +0300

    Use an enum class

--- src/main/kotlin/Question.kt
+++ src/main/kotlin/Question.kt
@@ -1,28 +1,33 @@
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 fun main() {
     val question1 = Question<String>(
         questionText = "Речка спятила с ума – По домам пошла сама. ___",
         answer = "водопровод",
-        difficulty = "medium"
+        Difficulty.MEDIUM
     )
 
     val question2 = Question<Boolean>(
         questionText = "Небо зелёное. Правда или ложь",
         answer = false,
-        difficulty = "easy"
+        Difficulty.EASY
     )
 
     val question3 = Question<Int>(
         questionText = "Сколько дней между полнолуниями?",
         answer = 28,
-        difficulty = "hard"
+        Difficulty.HARD
     )
 
     println(question1.answer)
     println(question2.answer)
     println(question3.answer)
+}
+enum class Difficult{
+    EASY,
+    MEDIUM,
+    HARD
 }
