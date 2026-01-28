3ed83923580761c019f60fca97d44903eac4770b
Mikailov05 <mikailov_akhmed@bk.ru>
2026-01-28 19:01:05 +0300

    Arrays in Kotlin

--- src/main/kotlin/Question.kt
+++ src/main/kotlin/Question.kt
@@ -1,79 +1,78 @@
-data class  Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
 )
-fun main() {
-    Quiz.printProgressBar()
-    val quiz = Quiz()
-    quiz.printQuiz()
-    val quiz = Quiz().apply { printQuiz() }
-}
-enum class Difficult{
+
+enum class Difficulty {
     EASY,
     MEDIUM,
     HARD
 }
-interface ProgressPrintable{
+
+interface ProgressPrintable {
     val progressText: String
     fun printProgressBar()
 }
 
-
-    class Quiz:ProgressPrintable{
-
-
+class Quiz : ProgressPrintable {
     val question1 = Question<String>(
         questionText = "Речка спятила с ума – По домам пошла сама. ___",
         answer = "водопровод",
-        Difficulty.MEDIUM
+        difficulty = Difficulty.MEDIUM
     )
 
     val question2 = Question<Boolean>(
         questionText = "Небо зелёное. Правда или ложь",
         answer = false,
-        Difficulty.EASY
+        difficulty = Difficulty.EASY
     )
 
     val question3 = Question<Int>(
         questionText = "Сколько дней между полнолуниями?",
         answer = 28,
-        Difficulty.HARD
+        difficulty = Difficulty.HARD
     )
-    companion object StudentProgress{
+
+    companion object StudentProgress {
         var total: Int = 10
-        var answered:Int = 3
+        var answered: Int = 3
     }
-    val Quiz.StudentProgress.progressText: String
-        get() = "${answered} of ${total} answered"
 
-        fun printQuiz() {
-            question1.let {
-                println(question1.questionText)
-                println(question1.answer)
-                println(question1.difficulty)
-            }
-            println()
-            question2.let {
-                println(question2.questionText)
-                println(question2.answer)
-                println(question2.difficulty)
-            }
-            println()
-            question3.let {
-                println(question3.questionText)
-                println(question3.answer)
-                println(question3.difficulty)
-            }
-            println()
+    override val progressText: String
+        get() = "${StudentProgress.answered} of ${StudentProgress.total} answered"
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
         }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
 
+    override fun printProgressBar() {
+        repeat(StudentProgress.answered) { print("▓") }
+        repeat(StudentProgress.total - StudentProgress.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
 }
-fun Quiz.StudentProgress.printProgressBar(){
 
-
-    repeat(times = Quiz.answered) {print(" ")}
-    repeat(times = Quiz.total - Quiz.answered) {print(" ")}
-    println()
-    println(Quiz.progressText)
+fun main() {
+    Quiz().printProgressBar()
+    val quiz = Quiz()
+    quiz.printQuiz()
 }
